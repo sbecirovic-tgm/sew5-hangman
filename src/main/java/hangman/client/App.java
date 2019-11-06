8957a408e0003fdcde764266232cf81ee9dd754a
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2019-11-06 14:47:44 +0100

    Created basic client structure

--- src/main/java/hangman/client/App.java
+++ src/main/java/hangman/client/App.java
@@ -1,11 +1,39 @@
 package hangman.client;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.InetAddressValidator;
+
 /**
  * @author Kacper Urbaniec
  * @version 2019-11-06
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        String address = "localhost";
+        Integer port = 8888;
+
+        if (args.length == 2) {
+            InetAddressValidator validator = new InetAddressValidator();
+            if (args[0].equals("localhost") ||validator.isValid(args[0])) {
+                address = args[0];
+            } else {
+                System.err.println("The given ip-address is invalid");
+                System.exit(1);
+            }
+            if (StringUtils.isNumeric(args[1])) {
+                port = Integer.parseInt(args[1]);
+            } else {
+                System.err.println("The given port is invalid");
+                System.exit(1);
+            }
+        }
+        else {
+            System.err.println("No port was given!");
+            System.exit(1);
+        }
+
+        ClientWorker client = new ClientWorker(address, port);
+        client.start();
+
     }
 }
--- src/main/java/hangman/client/ClientWorker.java
+++ src/main/java/hangman/client/ClientWorker.java
@@ -1,8 +1,53 @@
 package hangman.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * @author Kacper Urbaniec
  * @version 2019-11-06
  */
-public class ClientWorker {
+public class ClientWorker extends Thread {
+    private String host;
+    private Integer port;
+    private Socket socket;
+    private boolean listening;
+    private BufferedReader in;
+    private PrintWriter out;
+    private Scanner sc;
+
+    public ClientWorker(String host, Integer port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.socket = new Socket(host, port);
+            this.listening = true;
+            this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            this.out = new PrintWriter(this.socket.getOutputStream(), true);
+            this.sc = new Scanner(System.in);
+            while (listening) {
+                this.received();
+                this.send();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void received() {
+
+    }
+
+    private void send() {
+
+    }
 }
